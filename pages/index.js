27a42eee43464cc6797e6a5dfeeae9c232d67822
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-01-28 13:57:54 -0600

    Base Next.js template

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+
+    return (
+        <>
+            <Head>
+                <title>Next.js Template</title>
+                <meta name="description" content="Stephen Matheis' resume" />
+            </Head>
+            <div>
+                <h1>
+                    Next.js Template
+                </h1>
+            </div>
+        </>
+    )
+}
